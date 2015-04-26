130338ddc98b24ea96a6541f096ab6af9e9f914b
Wayne <dev.wayne.ix@gmail.com>
2015-04-26 16:59:48 +0800

    update

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
 
+var program = require('commander');
+var output = require('../lib/output');
 
+program
+    .version('0.0.1')
+    .parse(process.argv);
+
+
+output('mBase', false, 'less');
---
+++ gitConfig.js
@@ -0,0 +1,10 @@
+module.exports={
+	mBase:{
+		name:"mBase",
+		url:"https://github.com/devWayne/mBase",
+		folder:"mBase/src/modules/",
+		base:"base.less",
+		dist:""
+	}
+	
+}
